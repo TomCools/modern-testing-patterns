9fe3fa97a961d58a407922348c97fb3db680046a
Tom Cools <tom.cools@live.be>
2022-05-04 17:47:07 +0200

    Adjust Liquibase Container Updater to be more explicit
    We did some other stuff outside of the initial implementation.
    What is still not mentioned here is the cleanup of the "old" image, as we run a purge on all unused images every once in a while.

--- testcontainers/src/test/java/be/tomcools/advancedtestingpatterns/testcontainers/additional/LiquibaseContainerUpdater.java
+++ testcontainers/src/test/java/be/tomcools/advancedtestingpatterns/testcontainers/additional/LiquibaseContainerUpdater.java
@@ -1,26 +1,43 @@
 package be.tomcools.advancedtestingpatterns.testcontainers.additional;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.model.PushResponseItem;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.images.PullPolicy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class LiquibaseContainerUpdater {
 
-    private static DockerImageName IMAGE =  DockerImageName.parse("tomcools/postgres:dev")
-            .asCompatibleSubstituteFor("postgres");
+    /* NOTE
+    A blog post about this solution is in the works... not going to comment everything here in the meantime ;)
+     */
+
     @Container
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>(IMAGE)
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.2")
             .withImagePullPolicy(PullPolicy.alwaysPull())
+            // Custom waiter
+            .waitingFor((new LogMessageWaitStrategy())
+                    .withRegEx(".*database system is ready to accept connections.*\\s")
+                    .withTimes(1)
+                    .withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS))
+            )
+            // map container data to "non volume path" as only those are saved on commit.
+            .withEnv("PGDATA", "/var/lib/postgresql/container-data")
             .withDatabaseName("testcontainer")
             .withUsername("sa")
             .withPassword("sa");
@@ -39,7 +56,12 @@ public class LiquibaseContainerUpdater {
 
         // Push new image
         dockerClient.pushImageCmd("tomcools/postgres:dev")
-                .start().awaitCompletion();
+                .exec(new ResultCallback.Adapter<>() {
+                    @Override
+                    public void onNext(PushResponseItem object) {
+                        //log.info(object.toString());
+                    }
+                }).awaitCompletion();
     }
 
     @DynamicPropertySource
