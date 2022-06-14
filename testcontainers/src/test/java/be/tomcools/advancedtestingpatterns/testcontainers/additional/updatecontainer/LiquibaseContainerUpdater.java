8068218741fe36546191066fa7a9d4b0e5d29bcb
Tom Cools <tom.cools@live.be>
2022-06-14 20:19:02 +0200

    Add example in which we create a new Docker Image with data using TestContainers

--- testcontainers/src/test/java/be/tomcools/advancedtestingpatterns/testcontainers/additional/updatecontainer/LiquibaseContainerUpdater.java
+++ testcontainers/src/test/java/be/tomcools/advancedtestingpatterns/testcontainers/additional/updatecontainer/LiquibaseContainerUpdater.java
@@ -1,19 +1,28 @@
-package be.tomcools.advancedtestingpatterns.testcontainers.additional;
+package be.tomcools.advancedtestingpatterns.testcontainers.additional.updatecontainer;
 
+import be.tomcools.advancedtestingpatterns.app.velo.VeloRepository;
+import be.tomcools.advancedtestingpatterns.app.velo.VeloStation;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.model.AuthConfig;
 import com.github.dockerjava.api.model.PushResponseItem;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.images.PullPolicy;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -29,39 +38,42 @@ public class LiquibaseContainerUpdater {
 
     @Container
     public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.2")
-            .withImagePullPolicy(PullPolicy.alwaysPull())
-            // Custom waiter
-            .waitingFor((new LogMessageWaitStrategy())
-                    .withRegEx(".*database system is ready to accept connections.*\\s")
-                    .withTimes(1)
-                    .withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS))
-            )
             // map container data to "non volume path" as only those are saved on commit.
+            // original volume path is "/var/lib/postgresql/data"
             .withEnv("PGDATA", "/var/lib/postgresql/container-data")
             .withDatabaseName("testcontainer")
             .withUsername("sa")
             .withPassword("sa");
-
+    @Autowired
+    VeloRepository repo;
     @Test
     @Disabled("Should be run only with certain profiles ;)")
     public void pushNewImage() throws InterruptedException {
         // Startup and Liquibase happen first :-)
 
+        // You can even use some code to add data!
+        repo.save(VeloStation.builder().id("X").name("PRE_PROVISIONED").build());
+
         // Commit docker container changes into new image
         final DockerClient dockerClient = postgreSQLContainer.getDockerClient();
-        dockerClient.commitCmd(postgreSQLContainer.getContainerId())
+        final String result = dockerClient.commitCmd(postgreSQLContainer.getContainerId())
                 .withRepository("tomcools/postgres")
-                .withTag("dev")
+                .withTag("main")
                 .exec();
 
-        // Push new image
-        dockerClient.pushImageCmd("tomcools/postgres:dev")
+        //Push new image
+        final ResultCallback.Adapter<PushResponseItem> pushResult = dockerClient.pushImageCmd("tomcools/postgres:main")
                 .exec(new ResultCallback.Adapter<>() {
                     @Override
                     public void onNext(PushResponseItem object) {
-                        //log.info(object.toString());
+                        log.info(object.toString());
+                        if(object.isErrorIndicated()) {
+                            throw new RuntimeException("Failed push: " + object.getErrorDetail());
+                        }
                     }
-                }).awaitCompletion();
+                })
+                .awaitCompletion();
+        System.out.println(pushResult);
     }
 
     @DynamicPropertySource
