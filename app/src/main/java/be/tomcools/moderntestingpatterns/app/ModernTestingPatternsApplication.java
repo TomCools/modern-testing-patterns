fc5f72d90e4f9b46a180dabca56e0a4a2c5637c3
tomco <tom.cools@live.be>
2023-04-01 22:15:19 +0200

    Rebrand to Modern Testing Patterns

--- app/src/main/java/be/tomcools/moderntestingpatterns/app/ModernTestingPatternsApplication.java
+++ app/src/main/java/be/tomcools/moderntestingpatterns/app/ModernTestingPatternsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "be.tomcools.moderntestingpatterns")
-@EnableJpaRepositories(basePackages="be.tomcools.moderntestingpatterns.app.velo")
-@EntityScan(basePackages="be.tomcools.moderntestingpatterns.app.velo")
+@EnableJpaRepositories(basePackages="be.tomcools.moderntestingpatterns.app")
+@EntityScan(basePackages="be.tomcools.moderntestingpatterns.app")
 public class ModernTestingPatternsApplication {
 
     public static void main(String[] args) {
