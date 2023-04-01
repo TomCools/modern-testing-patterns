4fbdcd58e41699e09ccb6860413af9ccb90b6eb8
tomco <tom.cools@live.be>
2023-04-01 23:27:48 +0200

    Change contracts so they run on Classpath instead of LOCAL.
    This way, stubs don't need to be 'mvn install'-d.

--- spring-cloud-contract-consumer/src/test/java/be/tomcools/moderntestingpatterns/contract/ClientStubRunnerTest.java
+++ spring-cloud-contract-consumer/src/test/java/be/tomcools/moderntestingpatterns/contract/ClientStubRunnerTest.java
@@ -8,19 +8,22 @@ import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRun
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.web.client.RestTemplate;
 
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @SpringBootTest(classes = ModernTestingPatternsApplication.class)
 @AutoConfigureStubRunner(ids = "be.tomcools:spring-cloud-contract:+:stubs:8095",
-        stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+        stubsMode = StubRunnerProperties.StubsMode.CLASSPATH)
 public class ClientStubRunnerTest {
 
     @Test
-    public void testRunStub() {
-        final VeloStation resultingObject =
-                new RestTemplate().getForObject("http://localhost:8095/velo/001", VeloStation.class);
-
-        assertThat(resultingObject.getId()).isEqualTo("001");
+    public void givenVeloId_whenIGetTheDetailsOfThatVelo_idMatchesExpectedValue() {
+        when().
+                get("http://localhost:8095/velo/001").
+        then()
+                .statusCode(200)
+                .body("id", equalTo("001"));
     }
 
 }
